f9dc36edf53597c7549b8f134a6fd5041c99339b
MarceloGoncalvesBraga <85029465+MarceloGoncalvesBraga@users.noreply.github.com>
2022-05-03 20:16:16 -0300

    Create css.js

---
+++ css.js
@@ -0,0 +1,32 @@
+index.js:
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+
+const Header = () => {
+  return (
+    <>
+      <h1>Hello Style!</h1>
+      <p>Add a little style!.</p>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Header />);
+            
+            --------------------------
+  const Header = () => {
+  const myStyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Sans-Serif"
+  };
+  return (
+    <>
+      <h1 style={myStyle}>Hello Style!</h1>
+      <p>Add a little style!</p>
+    </>
+  );
+}
